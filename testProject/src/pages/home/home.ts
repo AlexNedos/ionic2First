b14352644ebff016d1428c12ae5d131cd46d3d92
Guru <anedost@gmail.com>
2016-12-21 00:17:09 +0200

    add toads and splash screen

--- testProject/src/pages/home/home.ts
+++ testProject/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController, ActionSheetController, AlertController } from 'ionic-angular';
+import { NavController, ActionSheetController, AlertController, ToastController } from 'ionic-angular';
 import { Http } from '@angular/http';
 
 @Component({
@@ -12,11 +12,13 @@ export class HomePage {
   constructor(public navCtrl: NavController,
     public http: Http,
     public actionSheetCtrl: ActionSheetController,
-    public alertCtrl: AlertController
+    public alertCtrl: AlertController,
+    public toastCtrl: ToastController
   ) { }
   items = [];
   page = 1;
   categoryType = '';
+
 
   presentAlert(aa) {
     let alert = this.alertCtrl.create({
@@ -56,27 +58,56 @@ export class HomePage {
 
     actionSheet.present();
   }
-
-  logForm(data, category) {
-    this.categoryUndefined(category);
-    this.page = 1;
-    let API_KEY = '2683000-931909b1d6f29b69f89649b0a';
-    let link = 'https://pixabay.com/api/?key=' + API_KEY + '&q=' + encodeURIComponent(data) + '&image_type=photo&pretty=true&page=1' + this.categoryType;
-    console.log(link);
-    this.http.get(link)
-      .subscribe(data => {
-        let dataParse = data.json();
-        this.items = [];
-        for (var i = 0; i < dataParse.hits.length; i++) {
-          this.items.push(dataParse.hits[i].webformatURL);
-        }
-      }, error => {
-
-        this.presentAlert(error.status)
-      //   console.log(error.status)
-        console.log("Oooops!");
+  // 400 - при скроле пусто
+  // 429 - нет картинок по запросу
+  // пустой статус - нет инета
+  presentToast(status) {
+     console.log(status)
+     var error;
+     if (status === 400) {
+         error = 'No more result'
+     } else if (status === null) {
+        error = 'No results'
+     } else if (status === 0) {
+         error = 'No internet connection'
+     } else if (status === undefined) {
+         error = 'Please enter something in the search'
+     }
+      let toast = this.toastCtrl.create({
+       message: error,
+       duration: 3000,
+       position: 'top'
       });
+      toast.present();
+    }
+  logForm(data, category) {
+     if (data === undefined) {
+         this.presentToast(data)
+     } else {
+        this.categoryUndefined(category);
+        this.page = 1;
+        let API_KEY = '2683000-931909b1d6f29b69f89649b0a';
+        let link = 'https://pixabay.com/api/?key=' + API_KEY + '&q=' + encodeURIComponent(data) + '&image_type=photo&pretty=true&page=1' + this.categoryType;
+        this.http.get(link)
+          .subscribe(data => {
+            let dataParse = data.json();
+            this.items = [];
+            if (dataParse.total === 0) {
+                this.presentToast(null)
+            } else {
+               this.items = [];
+               for (var i = 0; i < dataParse.hits.length; i++) {
+                 this.items.push(dataParse.hits[i].webformatURL);
+               }
+            }
+          }, error => {
+             console.log(error)
+            this.presentToast(error.status)
+          });
+     }
+
   }
+
 
   categoryUndefined(category) {
     if (category === undefined) {
@@ -102,12 +133,8 @@ export class HomePage {
               this.items.push(dataParse.hits[i].webformatURL);
             }
           }, error => {
-             this.presentAlert(error.status)
-            //  console.log(error.status)
-            // 400 - при скроле пусто
-            // 429 - нет картинок по запросу
-            // пустой статус - нет инета
-            console.log("Oooops!");
+              console.log(error)
+             this.presentToast(error.status)
           });
         infiniteScroll.complete();
       }, 500);
