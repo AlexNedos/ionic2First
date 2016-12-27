70abadc30fbcd2395990d8f790205b46e6b75bff
Guru <anedost@gmail.com>
2016-12-27 22:05:32 +0200

    new branch

--- testProject/src/app/app.module.ts
+++ testProject/src/app/app.module.ts
@@ -2,11 +2,14 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LibraryPage } from '../pages/library/library';
+import { Storage } from '@ionic/storage';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LibraryPage
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,8 +17,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LibraryPage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, Storage]
 })
 export class AppModule {}
--- testProject/src/pages/home/home.ts
+++ testProject/src/pages/home/home.ts
@@ -1,43 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
-import { NavController, ActionSheetController, AlertController, ToastController } from 'ionic-angular';
+import { Nav, NavController, ActionSheetController, ToastController, App, ViewController } from 'ionic-angular';
 import { Http } from '@angular/http';
+import { LibraryPage } from '../library/library';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController,
+  @ViewChild(Nav) nav: Nav;
+  constructor(
+    public navCtrl: NavController,
     public http: Http,
     public actionSheetCtrl: ActionSheetController,
-    public alertCtrl: AlertController,
-    public toastCtrl: ToastController
+    public toastCtrl: ToastController,
+    public viewCtrl: ViewController,
+    public appCtrl: App,
+    public storage: Storage
   ) { }
   items = [];
+  library = [];
   page = 1;
   categoryType = '';
 
+  openLibrary() {
+     this.appCtrl.getRootNav().push(LibraryPage);
+ }
 
-  presentAlert(aa) {
-    let alert = this.alertCtrl.create({
-      title: 'Low battery',
-      subTitle: aa,
-      buttons: ['Dismiss']
-    });
-    alert.present();
-  }
 
-  openPhoto() {
+  openPhoto(item) {
     let actionSheet = this.actionSheetCtrl.create({
-      // title: '',
       buttons: [
         {
           text: 'Add to library',
           role: 'destructive',
           handler: () => {
-            console.log('Destructive clicked');
+            this.storage.get('library').then((elem) => {
+              if (elem === null) {
+                this.library.push(item);
+                this.storage.set('library', this.library).then(() => {
+                });
+              } else {
+                 let checked = false;
+                for (let i = 0; i < elem.length; i++) {
+                    if (elem[i] === item) {
+                        checked = true;
+                    }
+                }
+                if (checked === false) {
+                   elem.push(item);
+                   this.storage.set('library', elem).then(() => {
+                  });
+                }
+              }
+            });
           }
         },
         {
@@ -58,53 +76,53 @@ export class HomePage {
 
     actionSheet.present();
   }
-  // 400 - при скроле пусто
-  // 429 - нет картинок по запросу
-  // пустой статус - нет инета
   presentToast(status) {
-     console.log(status)
-     var error;
-     if (status === 400) {
-         error = 'No more result'
-     } else if (status === null) {
-        error = 'No results'
-     } else if (status === 0) {
-         error = 'No internet connection'
-     } else if (status === undefined) {
-         error = 'Please enter something in the search'
-     }
-      let toast = this.toastCtrl.create({
-       message: error,
-       duration: 3000,
-       position: 'top'
-      });
-      toast.present();
+    console.log(status)
+    var error;
+    if (status === 400) {
+      error = 'No more result'
+    } else if (status === null) {
+      error = 'No results'
+    } else if (status === 0) {
+      error = 'No internet connection'
+    } else if (status === undefined) {
+      error = 'Please enter something in the search'
     }
+    let toast = this.toastCtrl.create({
+      message: error,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
+  }
   logForm(data, category) {
-     if (data === undefined) {
-         this.presentToast(data)
-     } else {
-        this.categoryUndefined(category);
-        this.page = 1;
-        let API_KEY = '2683000-931909b1d6f29b69f89649b0a';
-        let link = 'https://pixabay.com/api/?key=' + API_KEY + '&q=' + encodeURIComponent(data) + '&image_type=photo&pretty=true&page=1' + this.categoryType;
-        this.http.get(link)
-          .subscribe(data => {
-            let dataParse = data.json();
+    if (data === undefined) {
+      this.presentToast(data)
+    } else if (data === '') {
+      this.presentToast(undefined)
+    } else {
+      this.categoryUndefined(category);
+      this.page = 1;
+      let API_KEY = '2683000-931909b1d6f29b69f89649b0a';
+      let link = 'https://pixabay.com/api/?key=' + API_KEY + '&q=' + encodeURIComponent(data) + '&image_type=photo&pretty=true&page=1' + this.categoryType;
+      this.http.get(link)
+        .subscribe(data => {
+          let dataParse = data.json();
+          this.items = [];
+          if (dataParse.total === 0) {
+            this.presentToast(null)
+          } else {
             this.items = [];
-            if (dataParse.total === 0) {
-                this.presentToast(null)
-            } else {
-               this.items = [];
-               for (var i = 0; i < dataParse.hits.length; i++) {
-                 this.items.push(dataParse.hits[i].webformatURL);
-               }
+            for (var i = 0; i < dataParse.hits.length; i++) {
+              this.items.push(dataParse.hits[i].webformatURL);
             }
-          }, error => {
-             console.log(error)
-            this.presentToast(error.status)
-          });
-     }
+          }
+        }, error => {
+          console.log(error)
+          this.presentToast(error.status)
+        });
+    }
+
 
   }
 
@@ -133,8 +151,8 @@ export class HomePage {
               this.items.push(dataParse.hits[i].webformatURL);
             }
           }, error => {
-              console.log(error)
-             this.presentToast(error.status)
+            console.log(error)
+            this.presentToast(error.status)
           });
         infiniteScroll.complete();
       }, 500);
